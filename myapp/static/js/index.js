32cf6e3dba7b4cad4cf0c8bb87b813d2765527a5
insomniac12 <shivi.bajpai1@gmail.com>
2016-03-24 10:28:31 -0700

    Changes to be committed:

--- myapp/static/js/index.js
+++ myapp/static/js/index.js
@@ -1,15 +1,16 @@
-
-    var openFile = function(event) {
-     input = event.target;
+var openFile = function(event)
+ {
+    input = event.target;
     var reader = new FileReader();
-    reader.onload = function(){
-      var dataURL = reader.result;
-      var output = document.getElementById('output');
-      output.src = dataURL;
-    };
+    reader.onload = function()
+    {
+    var dataURL = reader.result;
+    var output = document.getElementById('output');
+    output.src = dataURL;
+   };
    reader.readAsDataURL(input.files[0]);
     name=input.files[0].name;
-  };
+ };
  
     var allVals = [];
     var pipeline=[];
@@ -17,40 +18,47 @@
     var i=0;
     var j=0;
     var l=0;
-       var func=function(name)
-   {
+    
+var func=function(name)
+ {
     allVals.push(name);
     
-   };
-    var doit = function(id,val){
-        
-        $('#'+id).click(function(){
-    for(j=0;j<val.length;j++){
-    if (val[j].length>0) {
-    allVals[j]=val[j];
-    }
-    else{
-    break;}
-    }
-            });
+ };
+    
+var doit = function(id,val)
+{
+    $('#'+id).click(function()
+         {
+            for(j=0;j<val.length;j++)
+                 {
+                    if (val[j].length>0)
+                    {
+                      allVals[j]=val[j];
+                    }
+                     else
+                     {
+                     break;
+                     }
+                 }
+         });
       return;
-    };
+ };
     
   
-   var cl=function()
+var cl=function()
    {
     allVals=[];
-  //  alert(allVals);
    };
-   var pipeline_temp=[];
-   var x;
-   var save_temp_pipe=function()
-   {
-    
+   
+
+var pipeline_temp=[];
+var x;
+   
+var save_temp_pipe=function()
+{
     pipeline_temp[k]=new Array(10);
     for (j=0;j<allVals.length;j++)
     {
-        
         pipeline_temp[k][j]=allVals[j];
     }
     option=document.createElement("option");
@@ -60,90 +68,93 @@
     select.appendChild(option);
     doit(k,pipeline_temp[k]);
     k=k+1;
-    //for (x=0;x<pipeline_temp.length;x++)
-// alert("pipeline"+ x + "is" + pipeline_temp[x]);
-   }
+};
+
 var z;
-   var save_pipe=function()
-   {
+   
+var save_pipe=function()
+{
     pipeline[i]=new Array(10);
     for (l=0;l<allVals.length;l++)
     {
-        
         pipeline[i][l]=allVals[l];
     }
  i=i+1;
-     $.ajax({
-        type:"POST",
-        url:"/pipe/",
-        data:{"pipeline":JSON.stringify(pipeline)},
-        success:function(json){
-    // alert(json.test);
-     //alert(json.index[0]);
-     $.each(json.index,function(){
-            option=document.createElement("option");
-    select=document.getElementById("demo");
-    text=document.createTextNode(this);});
-    option.id=z;
-    option.appendChild(text);
-    select.appendChild(option);
-     z=z+1;
-           // i=i+1;
-            },
-        error:function()
+ 
+     $.ajax(
             {
-                alert("bye");
+                type:"POST",
+                url:"/pipe/",
+                data:{"pipeline":JSON.stringify(pipeline)
+            },
+            
+            success:function(json)
+            {
+                $.each(json.index,function()
+                 {
+                    option=document.createElement("option");
+                    select=document.getElementById("demo");
+                    text=document.createTextNode(this);
+                 });
                 
-            }
+                option.id=z;
+                option.appendChild(text);
+                select.appendChild(option);
+                z=z+1;
+            },
+               
+                error:function()
+                {
+                  alert("bye");
+                }
         
-    });
-   };
+           });
+};
 
   
-  var run =  function(e){
-    
-    e.preventDefault();
-       //allVals = []
-    $.ajax({  
-         type: "POST",  
-         url: "/create_post/",
-         data:{"allVals":JSON.stringify(allVals),"name":name},
-         success: function(response){
-            for (a=0;a<response.url.length;a++) {
-                         
-          img=document.createElement("img");
-         // alert(response.url[a]);
-          img.setAttribute("src","/static/images/"+response.url[a])
-            document.getElementById("photo").appendChild(img)
-            }
+var run =  function(e)
+{
+  e.preventDefault();
+    $.ajax(
+           {  
+             type: "POST",  
+             url: "/create_post/",
+             data:{"allVals":JSON.stringify(allVals),"name":name},
+             
+            success: function(response)
+            {
+              for (a=0;a<response.url.length;a++)
+                {
+                 img=document.createElement("img");
+                 img.setAttribute("src","/static/images/"+response.url[a])
+                 document.getElementById("photo").appendChild(img)
+                }
         
-         },
+            },
          
-         error: function(e){  
-             alert("no");
-         },
-     });
-    
-  };
-  var res=[];
-  var l;
+              error: function(e)
+               {  
+                 alert("no");
+               },
+            });
+};
+  
+var res=[];
+var l;
+
 var func_pipe=function()
 {
   v=$('#demo').val();
-v=v.split(',');
-  for (i=0;i<v.length;i++) {
+  v=v.split(',');
+  for (i=0;i<v.length;i++)
+  {
     l=v[i].length;
-
-         res[i]=v[i].substring(3,l-1);
-        
-    
-    if (res[i]!='n' && res[i]!='ne') {
-       
-        allVals[i]=res[i]
-        
-    }
+    res[i]=v[i].substring(3,l-1);
+        if (res[i]!='n' && res[i]!='ne')
+        {
+            allVals[i]=res[i]
+        }
   }
 
-  
 };
 
--- myapp/views.py
+++ myapp/views.py
@@ -5,14 +5,11 @@ from  .models import pipelines
 import cv2
 import json
 import numpy as np
-#from matplotlib import pyplot as plt
 
-# Create your views here.
+
 def myview(request):
     template_name='index.html'
-
     data=pipelines.objects.all()
-    
     return render(request,template_name,{'data':data})
     
 @csrf_exempt
@@ -26,12 +23,15 @@ def create_post(request):
             
         response_data={}
         response_data['url']=[];
+        
         for i in op:
+            
             if i=="resize":
                 r=100.0/image.shape[1]
                 dim=(100,int(image.shape[0]*r))
                 image=cv2.resize(image,(0,0),fx=0.5,fy=0.5)
                 name="resized"+name
+            
             elif i=="blur":
                 image=cv2.blur(image,(5,5))
                 name="blur"+name
@@ -48,24 +48,22 @@ def create_post(request):
             cv2.imwrite(s+name,image)
             response_data['url'].append(name)    
         return HttpResponse(json.dumps(response_data),content_type="application/json")
+  
    
 @csrf_exempt         
 def pipe(request):
     if request.method=='POST':
          op2=request.POST.get('pipeline')
-        # print op2
          op2=json.loads(op2)
+         
          for i in op2:
             p=pipelines(pipeline_data=i)
             p.save()
+            
          arr={}
          data=pipelines.objects.all()
-         #print data
          arr['index']=[]
          for i in data :
             arr['index'].append(i.pipeline_data)
-            #print arr['index']
+
     return HttpResponse(json.dumps(arr),content_type="application/json")
-
-
-    
